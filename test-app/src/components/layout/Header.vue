553bfa997fe36cfa737eb50e0dcfe9684e1ae8f9
dbybanez <dbybanez@gmail.com>
2020-08-19 00:28:55 +0800

    minor changes to gulp file

---
+++ test-app/src/components/layout/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="header">
+    <h1>Why</h1>
+    <div id="nav">
+      <router-link to="/"><home-icon />Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <div>What is this</div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
--- test-app/src/main.js
+++ test-app/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-require('./assets/css/custom-bootstrap.min.css')
-require('./assets/css/custom-style.min.css')
+import './assets/css/custom-bootstrap.min.css'
+import './assets/css/custom-style.min.css'
+// require('./assets/css/custom-bootstrap.min.css')
+// require('./assets/css/custom-style.min.css')
 
 Vue.config.productionTip = false
 
